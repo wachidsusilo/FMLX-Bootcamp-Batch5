3b35325502a9719a2ab3a4d93396b94f6dda290d
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-04 11:51:06 +0700

    Initial commit

---
+++ Project1/Cat.cs
@@ -0,0 +1,26 @@
+﻿namespace Animal;
+
+public class Cat
+{
+    public string? Name;
+    public string? Color;
+    public string? Gender;
+    public int? Age;
+    public string? Race;
+    public bool? IsStripe;
+
+    public void Meow()
+    {
+        Console.WriteLine("Meow");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eat");
+    }
+
+    public void Jump()
+    {
+        Console.WriteLine("Jump");
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Project1;
+using Animal;
+
+static class Program
+{
+    static void Main()
+    {
+        var miu = new Cat();
+        
+        miu.Meow();
+        miu.Eat();
+        miu.Jump();
+        
+    }
+}
