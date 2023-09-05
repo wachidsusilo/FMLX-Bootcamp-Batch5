bf4446f5ebe02e8dae635e8e241f576e30d15930
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-05 11:44:18 +0700

    Create `Car` class

---
+++ Project2/Automotive/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace Project2.Automotive;
+using Part;
+
+public class Car
+{
+    public string? Manufacturer;
+    public string? Name;
+    public Engine? Engine;
+    public Tire? Tire;
+    public ILamp? Lamp;
+
+    public Car(
+        string? manufacturer = null, 
+        string? name = null, 
+        Engine? engine = null, 
+        Tire? tire = null, 
+        ILamp? lamp = null)
+    {
+        Manufacturer = manufacturer;
+        Name = name;
+        Engine = engine;
+        Tire = tire;
+        Lamp = lamp;
+    }
+
+    public override string ToString()
+    {
+        return $"Car({Manufacturer}, {Name}, {Engine}, {Tire}, {Lamp})";
+    }
+}
