9a0cd1d4102bdef6f21180d20b81e21ab6af20c2
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-05 11:43:49 +0700

    Create `Lamp` related classes implementing `ILamp` interface

---
+++ Project2/Automotive/Part/Lamp.cs
@@ -0,0 +1,61 @@
+﻿namespace Project2.Automotive.Part;
+
+public interface ILamp
+{
+    public void TurnOn();
+    public void TurnOff();
+}
+
+public class HalogenLamp : ILamp
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Halogen Lamp Turned On");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Halogen Lamp Turned Off");
+    }
+
+    public override string ToString()
+    {
+        return "HalogenLamp";
+    }
+}
+
+public class FluorescentLamp : ILamp
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Fluorescent Lamp Turned On");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Fluorescent Lamp Turned Off");
+    }
+
+    public override string ToString()
+    {
+        return "FluorescentLamp";
+    }
+}
+
+public class IncandescentLamp : ILamp
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Incandescent Lamp Turned On");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Incandescent Lamp Turned Off");
+    }
+
+    public override string ToString()
+    {
+        return "IncandescentLamp";
+    }
+}
