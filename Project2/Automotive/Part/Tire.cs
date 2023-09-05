c01a6e56a5401d87c529699a86771d8307c6572e
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-05 11:44:01 +0700

    Create `Tire` class

---
+++ Project2/Automotive/Part/Tire.cs
@@ -0,0 +1,20 @@
+﻿namespace Project2.Automotive.Part;
+
+public class Tire
+{
+    public string? Manufacturer;
+    public float Size;
+    public string? Type;
+
+    public Tire(string? manufacturer = null, float size = 0, string? type = null)
+    {
+        Manufacturer = manufacturer;
+        Size = size;
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"Tire({Manufacturer}, {Size}, {Type})";
+    }
+}
