4eadd32076c0fffe7dd4d8c29e53065138144948
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-05 11:13:55 +0700

    Move `Cat.cs` file to `Animal` folder

--- Project1/Animal/Cat.cs
+++ Project1/Animal/Cat.cs
@@ -1,19 +1,17 @@
-﻿namespace Animal;
+﻿namespace Project1.Animal;
 
 public class Cat
 {
-    public string? Name;
-    public string? Color;
-    public string? Gender;
-    public int? Age;
-    public string? Race;
-    public bool? IsStripe;
-
+    public string Name;
+    public string Color;
+    public string Gender;
+    public int Age;
+    public string Race;
+    public bool IsStripe;
     public void Meow()
     {
         Console.WriteLine("Meow");
     }
-
     public void Eat()
     {
         Console.WriteLine("Eat");
