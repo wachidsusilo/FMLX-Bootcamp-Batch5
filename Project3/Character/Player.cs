fafe947ec35e84dbfcdfb584b6d2ebc012362231
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-06 12:00:21 +0700

    Create `Player` class

---
+++ Project3/Character/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace Project3.Character;
+
+public class Player : Character
+{
+    public Player(long id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+    protected override void OnStartAttack(Character target)
+    {
+        base.OnStartAttack(target);
+        Console.WriteLine($"Player {Name} is playing sword_swing animation");
+    }
+
+    protected override void OnAttacked(Character actor, float damage)
+    {
+        base.OnAttacked(actor, damage);
+        Console.WriteLine($"Player {Name} is playing blood_splash animation");
+    }
+
+    protected override void OnDied(Character actor)
+    {
+        base.OnDied(actor);
+        Console.WriteLine($"Player {Name} is playing fall_down animation");
+    }
+
+    public override string ToString()
+    {
+        return $"Player(Id: {GetId()}, Name: {Name}, Hp: {Hp}, MaxHp: {MaxHp}, Atk: {Atk}, Def: {Def})";
+    }
+}
