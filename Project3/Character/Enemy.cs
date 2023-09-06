066552c70c5e2942488405c2ae975bc488dfb1fe
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-06 12:00:33 +0700

    Create `Enemy` class

---
+++ Project3/Character/Enemy.cs
@@ -0,0 +1,32 @@
+﻿namespace Project3.Character;
+
+public class Enemy : Character
+{
+    public Enemy(long id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+    protected override void OnStartAttack(Character target)
+    {
+        base.OnStartAttack(target);
+        Console.WriteLine($"Enemy {Name} is playing sword_swing animation");
+    }
+
+    protected override void OnAttacked(Character actor, float damage)
+    {
+        base.OnAttacked(actor, damage);
+        Console.WriteLine($"Enemy {Name} is playing blood_splash animation");
+    }
+
+    protected override void OnDied(Character actor)
+    {
+        base.OnDied(actor);
+        Console.WriteLine($"Enemy {Name} is playing fall_down animation");
+    }
+    
+    public override string ToString()
+    {
+        return $"Enemy(Id: {GetId()}, Name: {Name}, Hp: {Hp}, MaxHp: {MaxHp}, Atk: {Atk}, Def: {Def})";
+    }
+}
