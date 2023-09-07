d81e95009ff1cd54b281dd1c5b1509c5120c662d
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-07 12:08:46 +0700

    Implement entry point for Project4

---
+++ Project4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Project4.Controller;
+
+namespace Project4;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var playerController = new PlayerController();
+        var petController = new PetController();
+        var towerController = new TowerController();
+        
+        Console.WriteLine();
+        Console.WriteLine(playerController);
+        Console.WriteLine(petController);
+        Console.WriteLine(towerController);
+        
+        Console.WriteLine();
+        playerController.Walk(BaseController.DirectionForward);
+        playerController.Run(BaseController.DirectionLeft);
+        playerController.Stop();
+        playerController.Shoot();
+        
+        Console.WriteLine();
+        petController.Walk(BaseController.DirectionRight);
+        petController.Run(BaseController.DirectionForward);
+        petController.Stop();
+        petController.Shoot();
+        
+        Console.WriteLine();
+        towerController.Walk(BaseController.DirectionBackward);
+        towerController.Run(BaseController.DirectionLeft);
+        towerController.Stop();
+        towerController.Shoot();
+    }
+
+}
+
