fc022c66282cd95945eb936610947285bd6adeb1
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-07 12:08:25 +0700

    Create `TowerController` class

---
+++ Project4/Controller/TowerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Project4.Controller;
+
+public class TowerController : BaseController
+{
+    public override bool CanWalk()
+    {
+        return false;
+    }
+
+    public override bool CanRun()
+    {
+        return false;
+    }
+
+    public override bool CanShoot()
+    {
+        return true;
+    }
+
+    protected override void OnWalk(int direction)
+    {
+        throw new Exception("IllegalStateException: Tower cannot walk.");
+    }
+
+    protected override void OnRun(int direction)
+    {
+        throw new Exception("IllegalStateException: Tower cannot run.");
+    }
+
+    protected override void OnStop(int oldState, int direction)
+    {
+        Console.WriteLine($"Tower Stop from {StateToString(oldState)} {DirectionToString(direction)} - Animation");
+    }
+
+    protected override void OnShoot()
+    {
+        Console.WriteLine("Tower Shoot - Animation");
+    }
+
+    public override string ToString()
+    {
+        return new StringBuilder("TowerController(")
+            .Append($"State: {State} - {StateToString(State)}, ")
+            .Append($"Direction: {Direction} - {DirectionToString(Direction)}, ")
+            .Append($"CanWalk: {CanWalk()}, ")
+            .Append($"CanRun: {CanRun()}, ")
+            .Append($"CanShoot: {CanShoot()})")
+            .ToString();
+    }
+
+}
