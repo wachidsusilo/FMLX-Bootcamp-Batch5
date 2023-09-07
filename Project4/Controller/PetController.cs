554513d1b0a1507d63a9eb6b0dac5100127022fe
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-07 12:07:56 +0700

    Create `PetController` class

---
+++ Project4/Controller/PetController.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Project4.Controller;
+
+public class PetController : BaseController
+{
+    public override bool CanWalk()
+    {
+        return true;
+    }
+
+    public override bool CanRun()
+    {
+        return true;
+    }
+
+    public override bool CanShoot()
+    {
+        return false;
+    }
+
+    protected override void OnWalk(int direction)
+    {
+        Console.WriteLine($"Pet Walk {DirectionToString(direction)} - Animation");
+    }
+
+    protected override void OnRun(int direction)
+    {
+        Console.WriteLine($"Pet Run {DirectionToString(direction)} - Animation");
+    }
+
+    protected override void OnStop(int oldState, int direction)
+    {
+        Console.WriteLine($"Pet Stop from {StateToString(oldState)} {DirectionToString(direction)} - Animation");
+    }
+
+    protected override void OnShoot()
+    {
+        throw new Exception("IllegalStateException: Pet cannot shoot.");
+    }
+    
+    public override string ToString()
+    {
+        return new StringBuilder("PetController(")
+            .Append($"State: {State} - {StateToString(State)}, ")
+            .Append($"Direction: {Direction} - {DirectionToString(Direction)}, ")
+            .Append($"CanWalk: {CanWalk()}, ")
+            .Append($"CanRun: {CanRun()}, ")
+            .Append($"CanShoot: {CanShoot()})")
+            .ToString();
+    }
+    
+}
