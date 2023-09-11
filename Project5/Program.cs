b91c187f333060ce5a85acd55a4f61cbc3187f08
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-11 11:25:16 +0700

    Create Project5 implementation

---
+++ Project5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Project5;
+
+using Extension;
+using static Converter.Converter;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var isSucceed = TryParseDigit("12h4j520dkk0", out var result);
+        $"Parse digit {(isSucceed ? "success" : "failed")} with result: {result}".Print();
+        
+        isSucceed = TryParseDigit("abc", out result);
+        $"Parse digit {(isSucceed ? "succeeded" : "failed")} with result: {result}".Print();
+
+        var number = ParseNumberOrNull("abc");
+        $"Parse number: {number?.ToString(CultureInfo.InvariantCulture) ?? "null"}".Print();
+        
+        number = ParseNumberOrNull("12.45");
+        $"Parse number: {number?.ToString(CultureInfo.InvariantCulture) ?? "null"}".Print();
+    }
+}
