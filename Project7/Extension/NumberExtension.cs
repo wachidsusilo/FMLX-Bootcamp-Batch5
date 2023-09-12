8153abfb9072cca4419c77eb91701daec30c123e
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-12 10:55:21 +0700

    Create extension to convert instance which is implementing INumber to double

---
+++ Project7/Extension/NumberExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Project7.Extension;
+
+public static class NumberExtension
+{
+    public static double ToDouble<T>(this T value) where T: INumber<T>
+    {
+        return double.CreateTruncating(value);
+    }
+}
