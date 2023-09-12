ab8bf89c3a8f9ae27b0c8362ef9fb4d4eabc9e29
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-09-12 10:53:28 +0700

    Implement Project6

---
+++ Project6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Project6;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var fooBar = new FooBar.FooBar(15)
+        {
+            { 3, "foo" },
+            { 5, "bar" }
+        };
+
+        Console.WriteLine(fooBar.Execute()); 
+        // Output: 0, 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar
+
+        fooBar.Add(7, "moo");
+        fooBar.SetCount(30);
+
+        Console.WriteLine(fooBar.Execute()); 
+        // Output: 0, 1, 2, foo, 4, bar, foo, moo, 8, foo, bar, 11, foo, 13, moo, foobar, 16, 17, foo, 19, bar, foomoo, 22, 23, foo, bar, 26, foo, moo, 29, foobar
+    }
+}
