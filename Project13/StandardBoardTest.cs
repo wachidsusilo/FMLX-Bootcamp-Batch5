2ff2cf0b1e896abec5587d0d88cf280ff45b555d
Wachid Susilo <50637964+wachidsusilo@users.noreply.github.com>
2023-10-19 09:12:40 +0700

    Implement unit test for `StandardBoard` class

---
+++ Project13/StandardBoardTest.cs
@@ -0,0 +1,42 @@
+﻿using Project11.Chess.Boards;
+
+namespace Project13;
+
+public class StandardBoardTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void StandardBoard_GetNotationX_ReturnsCorrectResult()
+    {
+        var position = new Position(1,2);
+        var board = new StandardBoard();
+        var result = board.GetNotationX(position.X);
+        
+        Assert.That(result, Is.EqualTo("b"));
+    }
+
+    [Test]
+    public void StandardBoard_GetNotationY_ReturnsCorrectResult()
+    {
+        var position = new Position(1,2);
+        var board = new StandardBoard();
+        var result = board.GetNotationY(position.Y);
+        
+        Assert.That(result, Is.EqualTo("3"));
+    }
+
+    [Test]
+    public void StandardBoard_GetNotation_ReturnsCorrectResult()
+    {
+        var position = new Position(1,2);
+        var board = new StandardBoard();
+        var result = board.GetNotation(position);
+        
+        Assert.That(result, Is.EqualTo("b3"));
+    }
+
+}
